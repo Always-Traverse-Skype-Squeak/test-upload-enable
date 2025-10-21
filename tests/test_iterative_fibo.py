6573c12af6d61c43ba791b758ca801687652455e
fargito <fr.farge96@gmail.com>
2025-10-21 17:05:09 +0200

    remove iterative fibo 10

--- tests/test_iterative_fibo.py
+++ tests/test_iterative_fibo.py
@@ -2,11 +2,11 @@ import pytest
 from fibonacci import iterative_fibonacci
 
 
-@pytest.mark.benchmark(group="Iterative Fibo")
-def test_iterative_fibo_10(benchmark):
-    @benchmark
-    def _():
-        iterative_fibonacci(10)
+# @pytest.mark.benchmark(group="Iterative Fibo")
+# def test_iterative_fibo_10(benchmark):
+#     @benchmark
+#     def _():
+#         iterative_fibonacci(10)
 
 @pytest.mark.benchmark(group="Iterative Fibo")
 def test_iterative_fibo_25(benchmark):
